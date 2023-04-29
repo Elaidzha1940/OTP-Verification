143adcdbba418beec5f7a22e6a355a321f1337a9
Elaidzha1940 <eli080293@gmail.com>
2023-04-30 00:18:04 +0700

    OTP Verification

---
+++ OTP Verification/OTP Verification/OTP_VerificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  OTP_VerificationApp.swift
+//  OTP Verification
+//
+//  Created by Elaidzha Shchukin on 30.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct OTP_VerificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
